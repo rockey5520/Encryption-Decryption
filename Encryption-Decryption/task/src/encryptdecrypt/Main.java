3a3660f813661a5cf2a4558634e1539185f6d4b9
Rakesh Mothukuri <rockey5520@gmail.com>
2020-05-25 07:30:46 +0100

    incremental commit

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,7 +1,42 @@
 package encryptdecrypt;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+  public static void main(String[] args) {
+    String word = "we found a treasure!";
+    ArrayList<Character> straight = IntStream.rangeClosed('a', 'z').mapToObj(c -> (char) c)
+        .collect(Collectors.toCollection(ArrayList::new));
+    ArrayList<Character> reverse = (ArrayList<Character>) straight.clone();
+    Collections.reverse(reverse);
+
+    String[] s = word.split(" ");
+    List<String> finalStrings = new ArrayList<>();
+    for (String s1 : s
+    ) {
+      char[] chars = s1.toCharArray();
+      List<Character> charsReversed = new ArrayList<>();
+      for (Character character: chars
+      ) {
+          if(character != '!'){
+            int indexofCharacterinStriaght = straight.indexOf(character);
+            charsReversed.add(reverse.get(indexofCharacterinStriaght));
+          }else{
+            charsReversed.add('!');
+          }
+      }
+      finalStrings.add(charsReversed.stream().map(x->x.toString()).collect(Collectors.joining()));
     }
+    String encoded = finalStrings.stream().map(Object::toString).collect(Collectors.joining(" "));
+
+    System.out.println(encoded);
+  }
 }
